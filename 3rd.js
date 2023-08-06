b72202ca88da523b12a6166d8d2d6d3b450e76d0
Nino Londaridze <londaridzenino7@gmail.com>
2023-08-06 14:24:48 +0400

    updates

--- 3rd.js
+++ 3rd.js
@@ -1,6 +1,17 @@
 let firstInput = Number(prompt('Please, tell 1st number: '));
 let secondInput = Number(prompt('Please, tell 2nd number: '));
 let typeOfOperation = prompt('What would you like to perform?');
+
+while (
+    typeOfOperation !== '+' &&
+    typeOfOperation !== '-' &&
+    typeOfOperation !== '*' &&
+    typeOfOperation !== '/'
+) {
+    typeOfOperation = prompt('Enter operations');
+}
+
+
 let result;
 
 if (isNaN(firstInput) || isNaN(secondInput)) {
